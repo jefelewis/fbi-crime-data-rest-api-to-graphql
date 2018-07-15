8bdd2888d94991e318536f1540b316277d68c863
Jeff Lewis <jeffylew77@gmail.com>
2018-07-15 02:15:00 -0700

    [Dev] Adding more GraphQL Types

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,6 +1,7 @@
 // Imports: Node Fetch
 const fetch = require('node-fetch');
 
+// Imports: API Key
 import APIKEY from '/Users/jefflewis/Documents/Computer-Programming/Projects/Wrapped GraphQL REST APIs/fbi-crime-data-rest-api-to-graphql/config/config.js'
 
 
@@ -9,9 +10,24 @@ const RESOLVERS = {
   Query: {
     getArson: async (parent, args) => {
       const response = await 
-      fetch(`https://api.usa.gov/crime/fbi/sapi/api/arson/states/CA?api_key=nVpjUwkbXUEb3IQZDmThbZ1SLsBruEnBoZpxfxxj`);
+      fetch(`https://api.usa.gov/crime/fbi/sapi/api/arson/states/${args.stateAbbr}?api_key=${APIKEY}`);
       return response.json();
-    }
+    },
+    getPoliceEmployment: async (parent, args) => {
+      const response = await 
+      fetch(`https://api.usa.gov/crime/fbi/sapi/api/police-employment/states/${args.stateAbbr}?api_key=${APIKEY}`);
+      return response.json();
+    },
+    getAgency: async (parent, args) => {
+      const response = await 
+      fetch(`https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/${args.stateAbbr}?api_key=${APIKEY}`);
+      return response.json();
+    },
+    getAgencies: async (parent, args) => {
+      const response = await 
+      fetch(`https://api.usa.gov/crime/fbi/sapi/api/agencies/?api_key=${APIKEY}`);
+      return response.json();
+    },
   }
 };
 
--- server/graphql/types.js
+++ server/graphql/types.js
@@ -1,7 +1,10 @@
 // GraphQL: TypeDefs
 const TYPEDEFS = `
   type Query {
-    getArson(state_abbr: String): Arson
+    getArson(stateAbbr: String): Arson
+    getPoliceEmployment(stateAbbr: String): PoliceEmployment
+    getAgency(stateAbbr: String): Agency
+    getAgencies: Agency
   }
 
   type Arson {
@@ -16,6 +19,39 @@ const TYPEDEFS = `
     uninhabited: Int
     est_damage_value: Int
   }
+
+  type PoliceEmployment {
+    data_year: Int
+    civilian_ct: Int
+    female_civilian_ct: Int
+    female_officer_ct: Int
+    female_total_ct: Int
+    male_civilian_ct: Int
+    male_officer_ct: Int
+    male_total_ct: Int
+    population: Int
+    total_pe_ct: Int
+    state_id: Int
+    state_name: String
+    state_abbr: String
+    pe_ct_per_1000: Int
+  }
+
+  type Agency {
+    ori: String
+    agency_name: String
+    agency_type_name: String
+    state_name: String
+    state_abbr: String
+    division_name: String
+    region_name: String
+    region_desc: String
+    county_name: String
+    nibrs: Boolean
+    latitude: String
+    longitude: String
+  }
+
 `;
 
 
