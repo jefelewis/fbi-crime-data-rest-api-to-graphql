0618e4f562dcac0aa2d8124ef5e1ac131e15aaad
Jeff Lewis <jeffylew77@gmail.com>
2018-07-24 15:36:38 -0700

    [Dev] Adding files to the database

---
+++ server/database/download-participation-data.js
@@ -0,0 +1,81 @@
+// Imports: Node.js File System
+import fs from 'fs';
+
+// Imports: Axios
+import axios from 'axios';
+
+// Data: Geographic Data
+let regions = ["midwest", "northeast", "south", "west"];
+let states = [ "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
+
+
+// Retrieve Participation Data for the United States
+function downloadAllParticipationData() {
+  // Axios: GET Request
+  return axios({
+    method:'get',
+    url:`https://api.usa.gov/crime/fbi/sapi/api/participation/national?api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`,
+    responseType:'stream'
+  })
+  // Write response to .json file and store in the database
+  .then((response) => {
+    response.data.pipe(fs.createWriteStream(`/Users/jefflewis/Documents/Computer-Programming/Projects/Converted GraphQL REST APIs/fbi-crime-data-rest-api-to-graphql/database/participation-data/all-united-states/united-states-participation.json`))
+  })
+  // Error Handling
+  .catch((error) => console.log(error));
+}
+
+
+// Retrieve Participation Data for the United States
+function downloadAllParticipationDataByRegion() {
+  // Iterate through the Regions
+  for(let i = 0; i < regions.length; i++) {
+    let region = regions[i];
+    
+    // Axios: GET Request
+    axios({
+      method:'get',
+      url:`https://api.usa.gov/crime/fbi/sapi/api/participation/regions/${region}?api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`,
+      responseType:'stream'
+    })
+    // Write response to .json file and store in the database
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream(`/Users/jefflewis/Documents/Computer-Programming/Projects/Converted GraphQL REST APIs/fbi-crime-data-rest-api-to-graphql/database/participation-data/regions/${region}-participation.json`))
+    })
+    // Error Handling
+    .catch((error) => console.log(error));
+
+    // Set Timeout ()
+    
+  }
+}
+
+
+// // Retrieve Participation Data for the United States
+// function downloadAllParticipationDataByState() {
+//   // Iterate through the States
+//   for(let i = 0; i < states.length; i++) {
+//     let state = states[i];
+    
+//     // Axios: GET Request
+//     axios({
+//       method:'get',
+//       url:`https://api.usa.gov/crime/fbi/sapi/api/participation/states/${state}?api_key=api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`,
+//       responseType:'stream'
+//     })
+//     // Write response to .json file and store in the database
+//     .then((response) => {
+//       response.data.pipe(fs.createWriteStream(`/Users/jefflewis/Documents/Computer-Programming/Projects/Converted GraphQL REST APIs/fbi-crime-data-rest-api-to-graphql/database/participation-data/states/${state}-participation.json`))
+//     })
+//     // Error Handling
+//     .catch((error) => console.log(error));
+
+//     // Set Timeout
+//   }
+// }
+
+
+// Excute
+downloadAllParticipationData();
+downloadAllParticipationDataByRegion();
+// downloadAllParticipationDataByState();
